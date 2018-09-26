94c7d6c97e9c1374b3a81e039d2d7e64ffa310a3
Dave F <daveftech@gmail.com>
2018-09-26 16:23:55 -0500

    End of section 3

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -6,8 +6,10 @@ namespace DatingApp.API.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        
         public DbSet<Value> Values {get; set;}
 
-        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        public DbSet<User> Users {get; set;}        
     }
 }
